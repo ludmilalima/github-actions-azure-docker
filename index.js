2b68fe3664b362488d688719ed042a4272e84720
Talita <ludmilalima@outlook.com>
2021-11-28 23:47:24 -0300

    deploy with docker and heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World with Docker!');
 });
 
 app.listen(port, () => { console.log('Our App Is Up And Running!'); });
